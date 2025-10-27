bb12fac06b58a0a62f9a97051fe393ee4690110d
saifesebbarh50-cloud <s.a.i.f.e.s.e.b.b.a.r.h50@gmail.com>
2025-10-27 14:09:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   PORT: 3000,
   MONGO_URI: "mongodb://localhost:27017/saifhosting", // replace with your MongoDB Atlas URI
   SESSION_SECRET: "replace_with_session_secret",
-  JWT_SECRET: "replace_with_jwt_secret",
+  JWT_SECRET: "Kc2fP2O4ZjnrxDm-WLxQoRXuIqfXUPO7",
   DISCORD_CLIENT_ID: "1417083810230698034",
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || "replace_here",
+  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || "Kc2fP2O4ZjnrxDm-WLxQoRXuIqfXUPO7",
   DISCORD_CALLBACK_URL: "https://saif-hosting.onrender.com/auth/discord/callback",
   FRONTEND_URL: "https://saif-hosting.onrender.com",
   MAX_CPU_PER_USER: 200,
